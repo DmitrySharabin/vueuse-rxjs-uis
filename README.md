3e4ff7086d635d35a4faccf90c5ad12ddf0fa16e
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-09-17 16:31:03 +0200

    git init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rxjs Operating Heavily Dynamic Uis with Vue & VueUse
+
+Source repo: `https://github.com/BioPhoton/rxjs-operating-heavily-dynamic-uis`
+Video on YouTube: `https://youtu.be/XKfhGntZROQ`
---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+	<header>
+		<h1>
+			Operating Heavily Dynamic UIʼs
+		</h1>
+		<h2>Event Sourcing &amp; CQRS in the frontend</h2>
+	</header>
+	<main>
+		<output>0</output>
+
+		<section class="dashboard">
+			<button>Start</button>
+
+			<button>Pause</button>
+
+			<button>Set To</button>
+			<input type="number" min="0" />
+
+			<button>Reset</button>
+
+			<button>Count Up</button>
+
+			<button>Count Down</button>
+
+			<label>
+				Tick Speed
+				<input type="number" min="0" />
+			</label>
+
+			<label>
+				Count Diff
+				<input type="number" min="0" />
+			</label>
+		</section>
+	</main>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
